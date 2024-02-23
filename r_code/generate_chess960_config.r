bccf704c5989698694e255dd0331ddec8702ca7d
sarcasmgodgrownsore <109356192+sarcasmgodgrownsore@users.noreply.github.com>
2024-02-23 14:28:08 +1100

    Update generate_chess960_config.r

--- r_code/generate_chess960_config.r
+++ r_code/generate_chess960_config.r
@@ -4,14 +4,12 @@ ds <- c(1,3,5,7)
 ls <- c(2,4,6,8)
 config <- c("-","-","-","-","-","-","-","-")
 
-
 # First Rook
 fr <- which.max(runif(6))
 as <- as[as!=fr]
 ds <- ds[ds!=fr]
 ls <- ls[ls!=fr]
 config[fr] <- "r"
-
 
 # Second Rook
 asr <- as[as>fr+1]
@@ -21,7 +19,6 @@ ds <- ds[ds!=sr]
 ls <- ls[ls!=sr]
 config[sr] <- "r"
 
-
 # King
 ak <- as[as>1*fr & as<1*sr]
 k <- ak[which.max(runif(length(ak)))]
@@ -30,20 +27,17 @@ ds <- ds[ds!=k]
 ls <- ls[ls!=k]
 config[k] <- "k"
 
-
 # Light Square Bishop
 lsb <- ls[which.max(runif(length(ls)))]
 as <- as[as!=lsb]
 ls <- ls[ls!=lsb]
 config[lsb] <- "b"
 
-
 # Dark Square Bishop
 dsb <- ds[which.max(runif(length(ds)))]
 as <- as[as!=dsb]
 ds <- ds[ds!=dsb]
 config[dsb] <- "b"
-
 
 # First Knight
 fn <- as[which.max(runif(3))]
@@ -52,7 +46,6 @@ ds <- ds[ds!=fn]
 ls <- ls[ls!=fn]
 config[fn] <- "n"
 
-
 # Second Knight
 sn <- as[which.max(runif(2))]
 as <- as[as!=sn]
@@ -60,11 +53,9 @@ ds <- ds[ds!=sn]
 ls <- ls[ls!=sn]
 config[ls] <- "n"
 
-
 # Queen
 q <- 1*as
 config[q] <- "q"
-
 
 piece_configuration <- cat(config)
 
