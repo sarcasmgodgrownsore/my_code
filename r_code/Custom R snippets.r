5e0a9b6e4496c4a81ef37d253fe6a8653dda7b69
sarcasmgodgrownsore <109356192+sarcasmgodgrownsore@users.noreply.github.com>
2024-09-03 21:35:10 +1000

    Update Custom R snippets.r

--- r_code/Custom R snippets.r
+++ r_code/Custom R snippets.r
@@ -1,22 +1,5 @@
 # Custom code snippets
 
-snippet rml
-	rm(list=ls())
-
-snippet lj
-	${1:new_data} <- ${2:df1} %>%
-		left_join(${3:df2}, by = "${4:by_var}")
-
-snippet ij
-	${1:new_data} <- ${2:df1} %>%
-		inner_join(${3:df2}, by = "${4:by_var}")
-
-snippet rj
-	${1:new_data} <- ${2:df1} %>%
-		right_join(${3:df2}, by = "${4:by_var}")
-
-
-
 snippet rscript
 	# ${1:Description}
 	
