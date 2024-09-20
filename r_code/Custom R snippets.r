9a0cf2aa6e3f00e8fbc2399aeea7b08720804478
sarcasmgodgrownsore <109356192+sarcasmgodgrownsore@users.noreply.github.com>
2024-09-20 16:22:29 +1000

    Update Custom R snippets.r

--- r_code/Custom R snippets.r
+++ r_code/Custom R snippets.r
@@ -116,23 +116,40 @@ snippet shinymod
 	}
 
 
-# Custom code snippets
 
+
+# Custom code snippets (all environments)
 
 snippet rscript
+	# TITLE
+	
 	# ${1:Description}
+	#
+	#
 	
 	# Author: ${2:Name}
 	# Version: `r Sys.Date()`
 	
+	
 	# Packages
 	library(tidyverse)
 	
+	
+	# File paths
+	
+	input_path  <- ""
+	graph_path  <- ""
+	output_path <- ""
+	
+	
+	
 	# Parameters
+	
+	
 	
 	# ============================================================================
 	
-	${3:# Code}
+	# ${3: Code}
 
 snippet td
 	library(tidyverse)
@@ -186,12 +203,6 @@ snippet writec
 	write.csv(${1:new_data},
 		file = "${2:output_file}.csv",
 		append = FALSE)
-		
-snippet reads
-	saspath <- "${1:saspath}"
-	setwd(saspath)
-	new_data <- haven::read_sas("${2:in_ds}")
-
 
 snippet m2r
 	r1 <- c();	r2 <- c()
@@ -222,3 +233,19 @@ snippet m4r
 	c1 <- c();	c2 <- c();	c3 <- c();	c4 <- c()
 	${1:matrix_name} <- matrix(c(c1,c2,c3,c4), nrow = 4, ncol = 4, byrow = FALSE,
 		dimnames = list(c("r1","r2","r3","r4"),c("c1","c2","c3","c4")))
+
+
+# Custom code snippets (work only)
+
+snippet reads
+	saspath <- "${1:saspath}"
+	setwd(saspath)
+	new_data <- haven::read_sas("${2:in_ds}")
+
+
+# Custom code snippets (home only)
+
+snippet pack
+
+
+
