302dbb68ceb5be8cd7d611c15919732980dfefd1
sarcasmgodgrownsore <109356192+sarcasmgodgrownsore@users.noreply.github.com>
2024-09-24 16:39:15 +1000

    Update Python code examples.py

--- python_code/Python code examples.py
+++ python_code/Python code examples.py
@@ -1,9 +1,18 @@
 
 
 
-# Calculates compound interest
+# FUNCTION: Calculates compound interest
 def compound_interest(principal, rate, time):
     Amount = principal * (pow((1 + rate / 100), time))
     CI = Amount - principal
     print("Compound interest is", CI)
     print("Total amount is $", Amount)
+
+
+# CLASS: Book
+class Book:  
+    def __init__(self, title, author, genre, page_count):
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.page_count = page_count
